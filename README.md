86ae6d66d6773c80279966907f413211fc1829b0
David Richard <elidovrich.coding@gmail.com>
2022-09-11 15:34:28 -0400

    re-organize results section based on feedback

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # BizzBuzz Calculator WebApp
 
 ## Todo
-
+- in Results, change "Net Cost PU to Profit PU
 - if EQP is selected, make unit price auto-fill based on first value added (anywhere?)
 - the qtyInput array (in results) needs to be changed to auto update state based on qty-input (in input)  
 - fix profit margin "focus" behavior
--- src/components/results/Result.js
+++ src/components/results/Result.js
@@ -2,30 +2,23 @@ import { columnsArr } from "../../assets/helpers/helperArrays";
 import ProfitMargin from "./ProfitMargin";
 
 const Results = () => {
-  //the qtyInput array needs to be changed to auto update state based on qty-input 
-  const qtyInput = [50,100,250,500,1000,2500,5000];
+  //the qtyInput array needs to be changed to auto update state based on qty-input
+  const qtyInput = [50, 100, 250, 500, 1000, 2500, 5000];
   return (
     <div id="results-container">
       <h2>Results</h2>
       <div id="results-grid">
         {/* blank div is here in order to assist with the grid placement of "pcs-head" */}
         <div id="blank-div"></div>
-        {
-          columnsArr.map((column, i) => {
-            return <h5 className="pcs-head" id={"pcs-head-"+(i+1)} key={"pcs-head-"+(i+1)}>{qtyInput[i]+ " pcs"}</h5>
-          })
-        }
-        <h6 className="result-head">Net Cost PU:</h6>
-
         {columnsArr.map((column, i) => {
           return (
-            <div
-              className="result-data net-cost-pu"
-              id={"net-cost-pu-" + (i + 1)}
-              key={"net-cost-pu-" + (i + 1)}
+            <h5
+              className="pcs-head"
+              id={"pcs-head-" + (i + 1)}
+              key={"pcs-head-" + (i + 1)}
             >
-              <p>data</p>
-            </div>
+              {qtyInput[i] + " pcs"}
+            </h5>
           );
         })}
 
@@ -55,6 +48,30 @@ const Results = () => {
           );
         })}
 
+        <h6 className="result-head">Profit Margin</h6>
+        {columnsArr.map((column, i) => {
+          return (
+            <ProfitMargin
+              id={"profit-margin-" + (i + 1)}
+              key={"profit-margin-" + (i + 1)}
+            />
+          );
+        })}
+
+        <h6 className="result-head">Profit PU:</h6>
+
+        {columnsArr.map((column, i) => {
+          return (
+            <div
+              className="result-data profit-pu"
+              id={"profit-pu-" + (i + 1)}
+              key={"profit-pu-" + (i + 1)}
+            >
+              <p>data</p>
+            </div>
+          );
+        })}
+
         <h6 className="result-head">Total Profit</h6>
         {columnsArr.map((column, i) => {
           return (
@@ -66,11 +83,6 @@ const Results = () => {
               <p>data</p>
             </div>
           );
-        })}
-
-        <h6 className="result-head">Profit Margin %</h6>
-        {columnsArr.map((column, i) => {
-          return <ProfitMargin id={"profit-margin-" + (i + 1)} key={"profit-margin-" + (i + 1)} />;
         })}
 
         {/* output-grid closer */}
